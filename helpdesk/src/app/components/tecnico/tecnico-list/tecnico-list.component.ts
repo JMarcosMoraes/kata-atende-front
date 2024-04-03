4e59081930a781bbd8dd63b482ed056eb6f87cbd
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-04-03 20:13:06 -0300

    Componente tecnico list

--- helpdesk/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ helpdesk/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnicos';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Teste Usuário',
+      cpf: '123.123.123.12',
+      email: 'teste@teste.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '03/04/2024'
+    }
+  ]
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
+
