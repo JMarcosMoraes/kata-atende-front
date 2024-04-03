f4edd10cc9cf8c4a64a75feb62ba60259677804e
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-04-03 17:10:44 -0300

    Html e css para comp. Home

--- helpdesk/src/app/app.module.ts
+++ helpdesk/src/app/app.module.ts
@@ -27,11 +27,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
