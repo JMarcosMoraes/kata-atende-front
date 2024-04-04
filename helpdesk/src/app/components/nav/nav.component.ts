159796df72c2a8610ec1fd13c90c9e8c76ee6c37
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-04-03 23:53:24 -0300

    Ajuste no login

--- helpdesk/src/app/components/nav/nav.component.ts
+++ helpdesk/src/app/components/nav/nav.component.ts
@@ -11,7 +11,8 @@ export class NavComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.router.navigate(['tecnicos']);
+    //this.router.navigate(['tecnicos']);
+    this.router.navigate(['home']);
   }
 
 }
