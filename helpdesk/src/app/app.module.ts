e15512df283948df01a820169ed8baecf5e25241
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-04-03 21:38:29 -0300

    Serviço de validação Login

--- helpdesk/src/app/app.module.ts
+++ helpdesk/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -64,7 +65,12 @@ import { LoginComponent } from './components/login/login.component';
     MatTableModule,
     MatIconModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    })
   ],
   
   bootstrap: [AppComponent]
