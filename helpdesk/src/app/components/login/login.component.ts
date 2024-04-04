fe46e577789a593b7839827698989638461d90bb
JMarcosMoraes <josemarcosmoraes@gmail.com>
2024-04-03 21:09:11 -0300

    VAlidação de formulários Login

--- helpdesk/src/app/components/login/login.component.ts
+++ helpdesk/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3))
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validaCampos(): boolean {
+    if(this.email.valid && this.senha.valid){
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
