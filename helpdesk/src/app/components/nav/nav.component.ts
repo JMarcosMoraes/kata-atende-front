d3339ae1c8c43b9d9e11ce8f8fd7ea76a33173ff
José Marcos <josemarcosmoraes@gmail.com>
2024-04-09 09:11:44 -0300

    Botões de Ação para Técnico

--- helpdesk/src/app/components/nav/nav.component.ts
+++ helpdesk/src/app/components/nav/nav.component.ts
@@ -17,7 +17,7 @@ export class NavComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.router.navigate(['tecnicos/create']);
+    this.router.navigate(['tecnicos']);
     //this.router.navigate(['home']);
   }
   logout(){
