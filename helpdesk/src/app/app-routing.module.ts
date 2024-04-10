db4df47a9fd2f60b6d0a4b1e88b2da47c48e3c3e
José Marcos <josemarcosmoraes@gmail.com>
2024-04-10 08:43:45 -0300

    Técnico-upadate

--- helpdesk/src/app/app-routing.module.ts
+++ helpdesk/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
        {path: 'home', component: HomeComponent},
        {path: 'tecnicos', component: TecnicoListComponent},
-       {path: 'tecnicos/create', component: TecnicoCreateComponent}
+       {path: 'tecnicos/create', component: TecnicoCreateComponent},
+       {path: 'tecnicos/update/:id', component: TecnicoUpdateComponent}
     ] 
 }];
 
--- helpdesk/src/app/app.module.ts
+++ helpdesk/src/app/app.module.ts
@@ -35,6 +35,7 @@ import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptor, AuthInterceptorProvider } from './interceptors/auth.interceptor';
 import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { NgxMaskModule } from 'ngx-mask';
+import { TecnicoUpdateComponent } from './components/tecnico/tecnico-update/tecnico-update.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { NgxMaskModule } from 'ngx-mask';
     HeaderComponent,
     TecnicoListComponent,
     LoginComponent,
-    TecnicoCreateComponent
+    TecnicoCreateComponent,
+    TecnicoUpdateComponent
   ],
   imports: [
     BrowserModule,
